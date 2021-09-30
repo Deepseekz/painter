8130ec55e14a2da9ceaec0a1bbdfe559ba19e450
COUETOUX Basile <basile.couetoux@univ-amu.fr>
2021-09-30 06:56:33 +0000

    Update FrenchPainter.java

--- src/main/java/painter/FrenchPainter.java
+++ src/main/java/painter/FrenchPainter.java
@@ -3,13 +3,18 @@ package painter;
 import javafx.scene.canvas.GraphicsContext;
 
 public class FrenchPainter {
+    GraphicsContext graphicsContext;
+
     public FrenchPainter(GraphicsContext graphicsContext) {
+        this.graphicsContext = graphicsContext;
     }
 
 
-    public void drawRectangle(double v, double v1, double v2, double v3) {
+    public void drawRectangle(double x, double y, double w, double h) {
+        //utiliser graphicsContext.strokeRectangle
     }
 
-    public void drawCircle(double v, double v1, double v2) {
+    public void drawCircle(double x, double y, double radius) {
+        //utiliser graphicsContext.strokeOval
     }
 }
